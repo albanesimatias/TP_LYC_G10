4e2f773ec4b1c3d6cd6e05188dc1f9d595457f66
Alba <albanesigaston@gmail.com>
2024-08-26 18:05:13 -0300

    tp-compilador

---
+++ lexer.py
@@ -0,0 +1,137 @@
+import ply.lex as lex
+from pathlib import Path
+
+reserved = {
+    'if': 'IF',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'init': 'INIT',
+    'float': 'FLOAT',
+    'int': 'INT',
+    'str': 'STR',
+    'read': 'READ',
+    'write': 'WRITE',
+    'or': 'OR',
+    'and': 'AND',
+    'suma_los_ultimos': 'SUMA_LOS_ULTIMOS'
+}
+
+tokens = [
+    'A_PARENTESIS',
+    'C_PARENTESIS',
+    'A_LLAVE',
+    'C_LLAVE',
+    'A_CORCHETE',
+    'C_CORCHETE',
+    'ASIGNACION',
+    'N_ENTERO',
+    'N_DECIMAL',
+    'N_BINARIO',
+    'CADENA',
+    'VARIABLE',
+    'MENORI',
+    'MAYORI',
+    'IGUALI',
+    'MENORQ',
+    'MAYORQ',
+    'SUMA',
+    'RESTA',
+    'DIVISION',
+    'MULTIPLICACION',
+    'COMA',
+    'PUNTO_Y_COMA',
+    'DOS_PUNTOS'
+] + list(reserved.values())
+
+
+# Expresiones regulares para TOKENS simples
+t_MENORI = r'\<\='
+t_MAYORI = r'\>\='
+t_IGUALI = r'\=\='
+t_MENORQ = r'\<'
+t_MAYORQ = r'\>'
+t_SUMA = r'\+'
+t_RESTA = r'-'
+t_MULTIPLICACION = r'\*'
+t_DIVISION = r'/'
+t_A_PARENTESIS = r'\('
+t_C_PARENTESIS = r'\)'
+t_A_LLAVE = r'\{'
+t_C_LLAVE = r'\}'
+t_A_CORCHETE = r'\['
+t_C_CORCHETE = r'\]'
+t_ASIGNACION = r'='
+t_COMA = r','
+t_PUNTO_Y_COMA = r';'
+t_DOS_PUNTOS = r':'
+
+
+# Expresiones regulares con codigo de accion (validaciones/errores/ignorar)
+def t_COMENTARIO(t):
+    r'\*-.*-\*'
+    pass
+
+
+def t_VARIABLE(t):
+    r'[a-zA-Z](\w|_|-)*'
+    t.type = reserved.get(t.value, 'VARIABLE')
+    return t
+
+
+def t_CADENA(t):
+    r'"(\w|\s)*"'
+    if len(t.value) > 40:
+        raise Exception('La cadena exed el limite de caracteres (MAX_40)')
+    return t
+
+
+def t_N_BINARIO(t):
+    r'(0|1)+b'
+    return t
+
+
+def t_N_DECIMAL(t):
+    r'-?\d+\.\d+'
+    if len(t.value) > 10:
+        raise Exception('Limite de 32bits para numeros decimales')
+    t.value = float(t.value)
+    return t
+
+
+def t_N_ENTERO(t):
+    r'-?\d+'
+    if len(t.value) > 5:
+        raise Exception('Limite de 16bits para numeros enteros')
+    t.value = int(t.value)
+    return t
+
+
+# Regla que cuenta la cantidad de lineas
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+# Ignorar tabulaciones y espacios
+t_ignore = ' \t'
+
+# Manejo de errores
+
+
+def t_error(t):
+    print(f"Illegal character {t.value[0]} at line: {t.lexer.lineno}")
+    t.lexer.skip(1)
+
+
+# Build the lexer
+lexer = lex.lex()
+
+# path = Path('./TESTS/test_01.txt')
+# data = path.read_text()
+# lexer.input('[var,"ho la",123]')
+
+# while True:
+#     token = lexer.token()
+#     if not token:
+#         break
+#     print(token)
---
+++ parser.py
@@ -0,0 +1,190 @@
+# BNF y Logica de generacion de la tabla de simbolos
+# Generar txt de tabla de simbolos
+# parser.out -> se genera solo
+
+from lexer import tokens  # Se importan los tokens generado previamente en el lexer
+import ply.yacc as yacc  # analizador sintactico
+
+SYMBOL_TABLE = {}
+
+
+def p_programa(p):
+    '''programa : sentencia programa
+                | sentencia
+    '''
+    if len(p) == 3:
+        p[0] = p[1] + p[2]
+    else:
+        p[0] = p[1]
+
+
+def p_sentencia(p):
+    '''sentencia : asignacion
+                 | iteracion
+                 | seleccion
+                 | bloque_declaracion
+                 | suma_los_ultimos
+    '''
+    p[0] = p[1]
+
+
+def p_bloque_declaracion(p):
+    '''bloque_declaracion : INIT A_LLAVE declaraciones  C_LLAVE
+    '''
+    p[0] = p[3]
+
+
+def p_declaraciones(p):
+    '''declaraciones : declaracion declaraciones
+                     | declaracion
+    '''
+    # reveer logica
+    p[0] = p[1]
+
+
+def p_declaracion(p):
+    '''declaracion : lista_variables DOS_PUNTOS tipo_dato
+                   | lista_variables DOS_PUNTOS tipo_dato declaracion
+    '''
+    p[0] = p[1]
+
+
+def p_lista_variables(p):
+    '''lista_variables : VARIABLE
+                       | VARIABLE COMA lista_variables
+    '''
+
+
+def p_tipo_dato(p):
+    '''tipo_dato : FLOAT
+                 | INT
+                 | STR
+    '''
+    p[0] = p[1]
+
+
+def p_seleccion(p):
+    '''seleccion : IF A_PARENTESIS condicion C_PARENTESIS bloque
+                 | IF A_PARENTESIS condicion C_PARENTESIS bloque ELSE bloque
+    '''
+    if p[3]:
+        p[0] = p[5]
+    else:
+        if len(p) >= 6:
+            p[0] = p[7]
+
+
+def p_iteracion(p):
+    '''iteracion : WHILE A_PARENTESIS condicion C_PARENTESIS bloque'''
+    if p[3]:
+        p[0] = p[6]
+
+
+def p_condicion(p):
+    '''condicion : condicion OR comparacion
+                 | condicion AND comparacion
+                 | comparacion
+    '''
+    if len(p) == 4:
+        match p[2]:
+            case 'or': p[0] = p[1] or p[3]
+            case 'and': p[0] = p[1] and p[3]
+    if len(p) == 2:
+        p[0] = bool(p[1])
+
+
+def p_comparacion(p):
+    '''comparacion : elemento comparador elemento
+                   | elemento
+    '''
+    if len(p) == 4:
+        match p[2]:
+            case '<=': p[0] = p[1] <= p[3]
+            case '>=': p[0] = p[1] >= p[3]
+            case '==': p[0] = p[1] == p[3]
+            case '>': p[0] = p[1] > p[3]
+            case '<': p[0] = p[1] < p[3]
+    if len(p) == 2:
+        p[0] = bool(p[1])
+
+
+def p_comparador(p):
+    '''comparador : MENORI
+                  | MENORQ
+                  | MAYORQ
+                  | MAYORI
+                  | IGUALI
+     '''
+
+    p[0] = p[1]
+
+
+def p_bloque(p):
+    '''bloque : A_LLAVE programa C_LLAVE'''
+    p[0] = p[2]
+
+
+def p_asignacion(p):
+    '''asignacion : VARIABLE ASIGNACION VARIABLE
+                  | VARIABLE ASIGNACION N_ENTERO
+                  | VARIABLE ASIGNACION N_DECIMAL
+                  | VARIABLE ASIGNACION CADENA
+                  | VARIABLE ASIGNACION N_BINARIO
+                  | VARIABLE ASIGNACION lista
+    '''
+    p[0] = p[3]
+
+
+def p_sumaLosUltimos(p):
+    '''suma_los_ultimos : SUMA_LOS_ULTIMOS  A_PARENTESIS N_ENTERO PUNTO_Y_COMA  lista  C_PARENTESIS
+    '''
+    lista = p[5]
+    n = len(lista)-p[3]
+    ultimos = lista[n:]
+    p[0] = 0
+    for num in ultimos:
+        p[0] += num
+
+
+def p_lista(p):
+    '''lista : A_CORCHETE elementos C_CORCHETE'''
+    p[0] = p[2]
+
+
+def p_elementos(p):
+    '''elementos : elementos COMA elemento
+                 | elemento'''
+    if len(p) == 4:
+        p[0] = p[1] + [p[3]]
+    else:
+        p[0] = [p[1]]
+
+
+def p_elemento(p):
+    '''elemento : N_ENTERO
+                | N_DECIMAL
+                | N_BINARIO
+                | VARIABLE
+                | CADENA
+    '''
+    p[0] = p[1]
+
+# Error rule for syntax errors
+
+
+def p_error(p):
+    print("Syntax error in input!")
+
+
+# Build the parser
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = input('yacc > ')
+    except EOFError:
+        break
+    if not s:
+        continue
+    result = parser.parse(s)
+    print(result)
