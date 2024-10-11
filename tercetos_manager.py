1949dadba52fba13eef12282d87ae0f2589a04cc
Alba <albanesigaston@gmail.com>
2024-10-11 00:56:11 -0300

    se agrego la clase manager

---
+++ tercetos_manager.py
@@ -0,0 +1,36 @@
+from terceto import Terceto
+
+
+class TercetosManager:
+    def __init__(self):
+        self.lista = list()
+        self.indice = int(0)
+
+    def obtener_indice(self):
+        return self.indice
+
+    def crear_terceto(self, valor1, valor2, valor3):
+        terceto_nuevo = Terceto(valor1, valor2, valor3)
+        self.lista.append(terceto_nuevo)
+        self.indice = self.indice + 1
+        return self.indice - 1
+
+    def insertar_terceto(self, terceto):
+        self.lista.append(terceto)
+        self.indice = self.indice + 1
+        return self.indice - 1
+
+    def actualizar_terceto(self, indice, valor):
+        self.lista[indice].actualizar_terceto(valor)
+
+    def print_tercetos(self):
+        for terceto in self.lista:
+            print(terceto)
+
+
+tm = TercetosManager()
+tm.crear_terceto('+', 2, 3)
+tm.crear_terceto('-', 2, 3)
+index = tm.crear_terceto('=', 'a', None)
+tm.actualizar_terceto(index, 4)
+tm.print_tercetos()
