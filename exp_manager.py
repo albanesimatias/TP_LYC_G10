55b6a594f0e09672d574e899a7513803269864c1
Alba <albanesigaston@gmail.com>
2024-11-09 02:05:35 -0300

    se refactorizo codigo

--- exp_manager.py
+++ exp_manager.py
@@ -1,44 +1,4 @@
-import re
-
-
-def is_index(elemento):
-    cad = str(elemento)
-    if '[' in cad:
-        return True
-    return False
-
-
-def is_id(elemento):
-    cad = str(elemento)
-    regex = r'[a-zA-Z](\w|_)*'
-    return re.match(regex, elemento)
-
-
-def is_cad(elemento):
-    cad = str(elemento)
-    regex = r'"(\w|\s)*"'
-    return re.match(regex, cad)
-
-
-def is_bin(elemento):
-    cad = str(elemento)
-    regex = r'(0|1)+b'
-    return re.match(regex, cad)
-
-
-def get_key(elemento):
-    if is_cad(elemento):
-        elemento = str(elemento).replace('"', '')
-        return '__'+str(elemento)
-    if is_bin(elemento):
-        return '_'+str(elemento)
-    if not is_id(str(elemento)):
-        return '_'+str(elemento)
-    return str(elemento)
-
-
-def is_none(tipo):
-    return tipo is None
+from utils import is_index, get_key, is_none
 
 
 class ExpMagager:
